6b6143a7be30e9893da66739de16fae9601f1c44
CVazquezOpalka <cristianvazquez80@gmail.com>
2024-02-17 02:25:45 -0300

    add mock data from noticias-data.ts, for rendering into project

---
+++ data/noticias-data.ts
@@ -0,0 +1,110 @@
+interface SeedData {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  published: boolean;
+  createdAt: Date;
+}
+
+export const noticias: SeedData[] = [
+  {
+    id: "1",
+    title:
+      "Organización sin fines de lucro alcanza récord de recaudación en campaña de donaciones para la investigación del cáncer",
+    description:
+      "Después de una intensa campaña de recaudación de fondos, una organización benéfica logra superar su meta para financiar la investigación del cáncer, recibiendo el apoyo de miles de donantes en todo el mundo.",
+    published: true,
+    createdAt: new Date(),
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: "2",
+    title:
+      "Comunidad se une para ayudar a víctimas de desastre natural con donaciones de suministros básicos",
+    description:
+      "Tras un devastador terremoto, la comunidad local y las organizaciones de ayuda internacional se unen para proporcionar alimentos, agua y refugio a las víctimas, recibiendo donaciones de todo el país.",
+    published: true,
+    createdAt: new Date(),
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: "3",
+    title:
+      "Celebridades se unen en evento benéfico virtual para recaudar fondos para la lucha contra el hambre innew Date()
+      "Reconocidas figuras del entretenimiento organizan un evento en línea para sensibilizar sobre el hambre infantil y recaudar fondos para programas de alimentación en comunidades desfavorecidas.",
+    published: true,
+    createdAt: new Date(),
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: "4",
+    title:
+      "Empresa tecnológica dona millones de dólares para apoyar la educación en países en desarrollo",
+    description:
+      "Una importante empresa del sector tecnológico anuncia una importante donación destinada a mejorar el acceso a la educación en países en desarrollo, con el objetivo de empoderar a las comunidades más vulnerables.",
+    published: true,
+    createdAt: new Date(),
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: "5",
+    title:
+      "Fundación entrega becas universitarias a estudiantes de bajos recursos gracias a generosas donaciones de la comunidad",
+    description:
+      "Una fundación dedicada a apoyar la educación superior anuncia la entrega de becas completas a varios estudiantes de bajos recursos, gracias a las donaciones recibidas de empresas y particulares.",
+    published: true,
+    createdAt: new Date(),
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: "6",
+    title:
+      "Rescate de animales en peligro recibe ayuda masiva de la comunidad tras llamado a donaciones",
+    description:
+      "Un refugio de animales en riesgo de cierre debido a la falta de fondos recibe una abrumadora respuesta de la comunidad, que responde al llamado a donaciones para salvar a los animales y mantener el refugio abierto.",
+    published: true,
+    createdAt: new Date(),
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: "7",
+    title:
+      "Artista dona obras de arte para recaudar fondos en apoyo a la conservación del medio ambiente",
+    description:
+      "Un reconocido artista comprometido con la conservación del medio ambiente dona varias de sus obras para una subasta benéfica, con el objetivo de recaudar fondos para organizaciones dedicadas a la protección de la naturaleza.",
+    published: true,
+    createdAt: new Date(),
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: "8",
+    title:
+      "Institución médica recibe donaciones de equipos de protección personal para trabajadores de la salud",
+    description:
+      "Ante la escasez de equipos de protección personal durante la pandemia, una institución médica recibe una avalancha de donaciones de mascarillas, guantes y batas, garantizando la seguridad de los trabajadores de la salud.",
+    published: true,
+    createdAt: new Date(),
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: "9",
+    title:
+      "Jóvenes emprendedores lanzan campaña de crowdfunding para financiar proyectos sociales en su comunidad",
+    description:
+      "Un grupo de jóvenes emprendedores inicia una campaña de crowdfunding para financiar proyectos sociales enfocados en el empoderamiento juvenil, la educación y el desarrollo comunitario en su localidad.",
+    published: true,
+    createdAt: new Date(),
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: "10",
+    title:
+      "Voluntarios se unen para limpiar playas y proteger el medio ambiente gracias a donaciones de suministros",
+    description:
+      "Una iniciativa comunitaria para limpiar playas y combatir la contaminación marina recibe donaciones de materiales y equipos necesarios, lo que permite a los voluntarios llevar a cabo actividades de limpieza de manera efectiva y sostenible.",
+    published: true,
+    createdAt: new Date(),
+    image: "https://via.placeholder.com/150",
+  },
+];
