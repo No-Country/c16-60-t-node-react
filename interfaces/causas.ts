458364d46a72fa0820173fa87065a2eca2025154
CVazquezOpalka <cristianvazquez80@gmail.com>
2024-02-20 18:56:58 -0300

    agregado reparacion de la isses

--- interfaces/causas.ts
+++ interfaces/causas.ts
@@ -1,10 +1,9 @@
 //TODO: crear interfas de donaciones
-import { DonationData } from "./donations";
+
 
 export interface CausasData {
   id: number;
   title: string;
   description: string;
   createdAt: Date;
-  donations?: DonationData[];
 }
--- interfaces/donations.ts
+++ interfaces/donations.ts
@@ -1,11 +1,10 @@
-import {CausasData} from '../interfaces/causas'
+
 
 export interface DonationsData {
   id: number;
   title: string;
   description: string;
   createdAt: Date;
-  cause?: CausasData;
   causesId?: number;
 }
 
