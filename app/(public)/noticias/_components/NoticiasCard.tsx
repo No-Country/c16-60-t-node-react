2e6a727f274035c668c7e6044d896ee851b47b9f
Nicolas <nicolas.esc100@gmail.com>
2024-02-22 13:43:29 -0300

    Pagina de noticias completada con diseño responsive

--- app/(public)/noticias/_components/NoticiasCard.tsx
+++ app/(public)/noticias/_components/NoticiasCard.tsx
@@ -1,38 +1,45 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Link from "next/link";
+import { NoticiasPopup } from "./NoticiasPopup";
+import { Separator } from "@/components/ui/separator";
 
 export const NoticiasCard = ({
   imagen,
   nombreImagen,
   titulo,
   descripcion,
-  link,
+  fecha,
 }: {
   imagen: string;
   nombreImagen: string;
   titulo: string;
   descripcion: string;
-  link: string;
+  fecha: string;
 }) => {
   return (
-    <div className='mt-4 mb-4 sm:p-4'>
+    <div className='flex flex-col items-center justify-between rounded-lg my-5 sm:bg-gray-200 sm:p-4'>
       <Image
         src={imagen}
         width={"500"}
         height={"500"}
-        alt={nombreImagen}></Image>
-      <h1 className='text-xl mt-3 font-bold'>{titulo}</h1>
-      <p className='text-base mt-3 mb-5 font-normal  leading-tight'>
-        {descripcion}
-      </p>
-      <Button variant='purple'>
-        <Link className='text-base font-medium' href={link}>
-          Leer mas
-        </Link>
-      </Button>
+        alt={nombreImagen}
+        className='rounded-md mb-3 sm:rounded-lg'
+      />
+      <Separator className='sm:hidden' />
+      <h1 className='text-xl leading-tight font-bold mt-3 sm:m-0'>{titulo}</h1>
+      <div className='line-clamp-2'>
+        <p className='text-base mt-3 font-normal leading-tight hidden sm:block'>
+          {descripcion}
+        </p>
+      </div>
+      <div className='flex justify-center mt-6'>
+        <NoticiasPopup
+          titulo={titulo}
+          descripcion={descripcion}
+          imagen={imagen}
+          nombreImagen={nombreImagen}
+          fecha={fecha}
+        />
+      </div>
     </div>
   );
 };
-
-//bg-custom-purple w-24 h-8 rounded flex justify-center align-middle
