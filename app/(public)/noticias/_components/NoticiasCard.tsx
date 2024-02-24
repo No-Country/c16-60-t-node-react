3a88761c82eb2c348f4216eba9f864ffb9cbf345
Nicolas <nicolas.esc100@gmail.com>
2024-02-23 21:03:25 -0300

    Modificacion de la pagina noticias

--- app/(public)/noticias/_components/NoticiasCard.tsx
+++ app/(public)/noticias/_components/NoticiasCard.tsx
@@ -7,7 +7,7 @@ export const NoticiasCard = (noticia: NoticiaData) => {
   return (
     <div className="flex flex-col items-start justify-between my-5 rounded-lg border p-5 w-full ">
       <Image src={image} width={"400"} height={"400"} alt={title} className="rounded-md" />
-      <h1 className="text-sm text-pretty lg:text-base line-clamp-2 font-medium  ">{title}</h1>
+      <h1 className="text-sm text-pretty lg:text-base line-clamp-2 font-medium my-4">{title}</h1>
       <NoticiasPopup {...noticia} />
     </div>
   );
