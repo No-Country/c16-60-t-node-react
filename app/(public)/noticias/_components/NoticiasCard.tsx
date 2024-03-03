84a0aed3df1148368581181e8585e616c25344ea
Nicolas <nicolas.esc100@gmail.com>
2024-03-03 17:52:12 -0300

    Mejora en la vista de las imagenes de la pagina de noticias

--- app/(public)/noticias/_components/NoticiasCard.tsx
+++ app/(public)/noticias/_components/NoticiasCard.tsx
@@ -5,9 +5,19 @@ import { NoticiasPopup } from "./NoticiasPopup";
 export const NoticiasCard = (noticia: NoticiaData) => {
   const { title, description, image, createdAt } = noticia;
   return (
-    <div className="flex flex-col items-start justify-between my-5 rounded-lg border p-5 w-full ">
-      <Image src={image} width={"400"} height={"400"} alt={title} className="rounded-md" />
-      <h1 className="text-sm text-pretty lg:text-base line-clamp-2 font-medium my-4">{title}</h1>
+    <div className='flex flex-col items-center sm:items-start justify-between my-5 rounded-lg border p-5 w-full '>
+      <div className='lg:h-[200px] flex justify-center'>
+        <Image
+          src={image}
+          width={"400"}
+          height={"200"}
+          alt={title}
+          className='rounded-md'
+        />
+      </div>
+      <h1 className='text-sm text-pretty lg:text-base line-clamp-2 font-medium my-4'>
+        {title}
+      </h1>
       <NoticiasPopup {...noticia} />
     </div>
   );
