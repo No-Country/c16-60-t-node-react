01c58af4e009596e50037d867a1b1336a3ab7639
LostoVayne <moises.barrera.devmail.com>
2024-03-05 18:34:07 -0300

    agregado el footer a los productos

--- app/(private)/products/layout.tsx
+++ app/(private)/products/layout.tsx
@@ -1,7 +1,14 @@
+import Footer from "../_components/Footer";
+
 export const metadata = {
   title: "SEO Title",
   description: "SEO Title",
 };
 export default function ProductLayout({ children }: { children: React.ReactNode }) {
-  return <div className="bg-[#FAFAFA] ">{children}</div>;
+  return (
+    <div className="bg-[#FAFAFA]">
+      {children}
+      <Footer />
+    </div>
+  );
 }
