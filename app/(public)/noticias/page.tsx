0bbf64a65b94a08eafee6066646c07e6e67a4a06
Nicolas <nicolas.esc100@gmail.com>
2024-03-04 23:16:50 -0300

    Titulo SmartHelp a las paginas desconocidad de la web, mejora en la vista mobile de noticias, Implementacion de la pagina politicas y agregar texto al navbar private

--- app/(public)/noticias/page.tsx
+++ app/(public)/noticias/page.tsx
@@ -5,7 +5,7 @@ import { NoticiasCard } from "./_components/NoticiasCard";
 const NoticiasPage = () => {
   return (
     <section className="w-full my-10">
-      <div className="grid grid-cols-1 gap-8 xl:gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full ">
+      <div className="grid grid-cols-1 gap-8 xl:gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full px-3">
         {noticias.map((noticia: NoticiaData) => (
           <NoticiasCard key={noticia.id} {...noticia} />
         ))}
