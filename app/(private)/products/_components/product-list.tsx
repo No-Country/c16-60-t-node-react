382abebb2fb1883dee3b8155af22c8c6dbeaba02
LostoVayne <moises.barrera.devmail.com>
2024-03-06 02:35:58 -0300

    agregada la data de los productos

--- app/(private)/products/_components/product-list.tsx
+++ app/(private)/products/_components/product-list.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { ProductsData } from "@/interfaces/productos";
 import { useCategoryState } from "@/store/category-state";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import ProductCard from "./productcard";
 
@@ -22,9 +21,7 @@ const ProductList = ({ products }: { products: ProductsData[] }) => {
     <div className="gap-3 3xl:gap-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 3xl:grid-cols-5">
       {productState.map((product) => (
         <div key={product.id}>
-          <Link href={`/product/${product.id}`}>
             <ProductCard product={product} />
-          </Link>
         </div>
       ))}
     </div>
