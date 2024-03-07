656e31da7f2f44951acaf30d03155693e786d429
LostoVayne <moises.barrera.devmail.com>
2024-03-07 15:47:00 -0300

    corregida la donacion correctamente

--- app/(private)/cart/page.tsx
+++ app/(private)/cart/page.tsx
@@ -6,11 +6,15 @@ import { PaymentProduct } from "@/interfaces/payment-product";
 import { useCartStore } from "@/store/cart-state";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import DataPayment from "./_components/data-payment";
 import ListProductInCart from "./_components/list-product-cart";
 
 const CartPage = () => {
+
+  const [loading, setLoading] = useState<boolean>(false);
   const CartProducts = useCartStore((state) => state.cart);
+  const TotalItems = useCartStore((state) => state.getTotalItems());
   const TotalPrice = useCartStore((state) => state.TotalPriceProducts());
   const valorEnvio = Math.round(TotalPrice * 0.5);
   const ProductsPayment: PaymentProduct[] = CartProducts.map((product) => ({
@@ -34,14 +38,23 @@ const CartPage = () => {
     ]);
   };
 
+
+  useEffect(() => {
+    setLoading(true);
+  },[])
+
+  if (!loading) {
+    return null;
+  }
+
   return (
-    <main className="flex lg:flex-row flex-col justify-center items-start gap-5 mx-auto lg:px-0 pt-16 pb-10 max-w-screen-xl h-[calc(100vh-75px)]">
+    <main className="flex lg:flex-row flex-col justify-center items-stretch lg:items-start gap-5 mx-auto px-5 lg:px-0 py-5 lg:pt-16 lg:pb-10 w-full lg:max-w-screen-xl h-auto lg:h-[calc(100vh-75px)]">
       <section className="flex-1">
         <Link href={"/products"} className="font-medium text-base underline">
           Continuar Comprando
         </Link>
-        <h2 className="my-2 font-semibold text-3xl text-gray-800">My Carrito de compras</h2>
-        <span>Tienes .... productos en tu carrito</span>
+        <h2 className="my-2 font-semibold text-gray-800 text-xl lg:text-3xl">My Carrito de compras</h2>
+        <span className="font-base text-gray-600">Tienes {TotalItems} productos en tu carrito</span>
         <div className="w-full h-auto max-h-[400px]">
           <ListProductInCart />
         </div>
